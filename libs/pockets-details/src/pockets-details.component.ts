40c188caf50e47f7c119a15e241a986a1d69e168
danielstevenberger <daniel.berger95@gmail.com>
2021-02-19 12:47:37 -0500

    Pockets web finished(mobile needs some work)

--- libs/pockets-details/src/pockets-details.component.ts
+++ libs/pockets-details/src/pockets-details.component.ts
@@ -1,20 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+export const mockPockets = [
+  {
+    id: '1a',
+    goal: 1500,
+    goalDate: '2021-04-20',
+    funds: 47.46,
+    name: 'Travel',
+    transactions: [
+      {
+        amount: '9.34',
+        account: 'Saving',
+        incoming: true,
+        date: '2019-11-8',
+      },
+      {
+        amount: '5.00',
+        account: 'Current',
+        date: '2019-07-12',
+      },
+      {
+        amount: '43.12',
+        account: 'Shared',
+        incoming: true,
+        date: '2019-07-12',
+      },
+    ],
+  },
+  {
+    id: '2b',
+    goal: null,
+    funds: 0,
+    name: 'Savings',
+  },
+  {
+    id: '3c',
+    goal: 500,
+    goalDate: '2021-04-19',
+    funds: 250,
+    name: 'Transportation',
+    transactions: [
+      {
+        amount: '250',
+        account: 'Saving',
+        incoming: true,
+        date: '2020-09-11',
+      },
+    ],
+  },
+];
 
 @Component({
-  selector: 'pt-pockets-details',
-  template: `
-    <p>
-      pockets-details works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: 'pt-individual-pocket',
+  templateUrl: `./pockets-details.component.html`,
 })
 export class PocketsDetailsComponent implements OnInit {
+  pockets: object[] = mockPockets;
+  selectedPocket: any;
+  pocket: any;
+  loading: boolean = false;
 
-  constructor() { }
+  @Input() pocketId = '';
+
+  @Output() goBack = new EventEmitter();
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.pocket = this.pockets.filter((obj: any) => {
+      return obj.id === this.pocketId;
+    })[0];
   }
 
+  goToPockets() {
+    this.goBack.emit('');
+  }
+
+  getProgress(id: string) {
+    //@ts-ignore
+    let progress = Math.ceil((this.pocket.funds / this.pocket.goal) * 100);
+    return progress > 100 ? 100 : progress;
+  }
+
+  groupTransactions() {
+    if (!this.pocket.transactions) return false;
+    return this.pocket.transactions.reduce((val: any, el: any) => {
+      val[el.date] = (val[el.date] || []).concat(el);
+      return val;
+    }, {});
+  }
 }
--- libs/pockets-details/src/pockets-details.module.ts
+++ libs/pockets-details/src/pockets-details.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackbaseCoreModule } from '@backbase/foundation-ang/core';
 import { PocketsDetailsComponent } from './pockets-details.component';
+import { BbDatePipeModule } from '@backbase/ui-ang';
+import { ButtonModule, IconModule, ModalModule } from '@backbase/ui-ang';
 
 @NgModule({
   declarations: [PocketsDetailsComponent],
   imports: [
     CommonModule,
     BackbaseCoreModule.withConfig({
-      classMap: { PocketsDetailsComponent }
-    })
-  ]
+      classMap: { PocketsDetailsComponent },
+    }),
+    ButtonModule,
+    IconModule,
+    ModalModule,
+    BbDatePipeModule,
+  ],
 })
-export class PocketsDetailsModule { }
+export class PocketsDetailsModule {}
