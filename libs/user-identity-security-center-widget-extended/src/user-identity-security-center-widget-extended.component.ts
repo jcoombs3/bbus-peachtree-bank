696c3fe91130cad328d929a6c762c3584f04a211
danielstevenberger <daniel.berger95@gmail.com>
2021-02-22 12:25:17 -0500

    Password custom validation

--- libs/user-identity-security-center-widget-extended/src/user-identity-security-center-widget-extended.component.ts
+++ libs/user-identity-security-center-widget-extended/src/user-identity-security-center-widget-extended.component.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { ManageProfilePreferencesService, UserDataService } from '@backbase/user-common';
+import { BehaviorSubject, ReplaySubject, Subject, Subscription } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
+
+function lowerCase(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    if (control.value) {
+      return /.*[a-z].*/.test(control.value) ? null : { lowerCase: control.value };
+    } else {
+      return null;
+    }
+  };
+}
+
+function upperCase(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null =>
+    /.*[A-Z].*/.test(control.value) ? null : { upperCase: control.value };
+}
+
+function digit(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null =>
+    /.*\d.*/.test(control.value) ? null : { digit: control.value };
+}
 
 @Component({
   selector: 'pt-user-identity-security-center-widget-extended',
-  template: `
-    <p>
-      user-identity-security-center-widget-extended works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: 'user-identity-security-center-widget-extended.component.html',
+  styles: [],
+  providers: [ManageProfilePreferencesService, UserDataService],
 })
-export class UserIdentitySecurityCenterWidgetExtendedComponent implements OnInit {
+export class UserIdentitySecurityCenterWidgetExtendedComponent implements OnInit, OnDestroy {
+  constructor() {}
 
-  constructor() { }
+  private formSubject = new BehaviorSubject<any>(null);
+  form!: Subscription;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getForm(form: FormGroup) {
+    this.formSubject.next(form);
+    this.formSubject.pipe(take(1)).subscribe((form) => {
+      form.controls.inputNewPassword.setValidators([Validators.minLength(6), lowerCase(), digit(), upperCase()]);
+    });
   }
 
+  reset() {
+    this.form.unsubscribe();
+  }
+
+  ngOnDestroy() {
+    this.form.unsubscribe();
+  }
 }
--- libs/user-identity-security-center-widget-extended/src/user-identity-security-center-widget-extended.module.ts
+++ libs/user-identity-security-center-widget-extended/src/user-identity-security-center-widget-extended.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackbaseCoreModule } from '@backbase/foundation-ang/core';
 import { UserIdentitySecurityCenterWidgetExtendedComponent } from './user-identity-security-center-widget-extended.component';
+import { UserIdentitySecurityCenterWidgetModule } from '@backbase/retail-ang/users';
+import { IconModule } from '@backbase/ui-ang';
 
 @NgModule({
   declarations: [UserIdentitySecurityCenterWidgetExtendedComponent],
   imports: [
     CommonModule,
     BackbaseCoreModule.withConfig({
-      classMap: { UserIdentitySecurityCenterWidgetExtendedComponent }
-    })
-  ]
+      classMap: { UserIdentitySecurityCenterWidgetExtendedComponent },
+    }),
+    UserIdentitySecurityCenterWidgetModule,
+    IconModule,
+  ],
 })
-export class UserIdentitySecurityCenterWidgetExtendedModule { }
+export class UserIdentitySecurityCenterWidgetExtendedModule {}
