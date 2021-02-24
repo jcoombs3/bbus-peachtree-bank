a0bef6bd52b9d420090db34f979907539fa7c5b0
danielstevenberger <daniel.berger95@gmail.com>
2021-02-24 16:57:22 -0500

    i18n need to translate a few things

--- apps/peachtree-retail-app/src/environments/environment.ts
+++ apps/peachtree-retail-app/src/environments/environment.ts
@@ -4,74 +4,74 @@ import { mockProviders } from '../mocks/providers';
 import { pageModel } from '../mocks/page-model';
 import { ContentType } from '@backbase/foundation-ang/web-sdk';
 
-const services: ExternalServices = {
-  auth: () => ({
-    login: (username: any, password: any) => Promise.resolve(),
-    logout: () => Promise.resolve(),
-    goToLoginPage: () => {},
-    register: (countdown: any) => () => {},
-    refresh: () => Promise.resolve(),
-    timeToLive: () => 100,
-  }),
-  eventBus: () => ({
-    publish: (eventName: any, data: any) => {
-      console.log(`eventBus published '${eventName}' with payload:`, data);
-    },
-    subscribe: (eventName: any, listener: any) => {
-      console.log(`eventBus subscribed '${eventName}' to listener:`, listener);
-    },
-    unsubscribe: (eventName: any, listener: any) => {
-      console.log(`eventBus unsubscribed '${eventName}' from listener:`, listener);
-    },
-  }),
-  navigation: () => ({
-    getBreadcrumbs: (uuid: any, depth: any) => {
-      return Promise.resolve({
-        type: 'externalLink',
-        title: 'Backbase',
-        url: 'http://www.backbase.com',
-        isCurrent: true,
-        properties: {},
-      });
-    },
-    getTree: (uuid: any, depth: any) => {
-      return Promise.resolve({
-        type: 'externalLink',
-        title: 'Backbase',
-        url: 'http://www.backbase.com',
-        isCurrent: true,
-        isInPath: false,
-        properties: {},
-        children: [],
-      });
-    },
-  }),
-  pageConfig: () => ({
-    apiRoot: '/gateway/api',
-    staticResourcesRoot: '/gateway/api/portal',
-    portalName: 'backbase-wc3',
-    pageName: 'index',
-    currentLink: '',
-    version: '6',
-    locale: 'en-US',
-  }),
-  portalContent: () => ({
-    get: () => {
-      return Promise.resolve({
-        type: ContentType.IMAGE,
-        contentRef: '/../../assets/logo-sml.svg',
-      });
-    },
-  }),
-};
+// const services: ExternalServices = {
+//   auth: () => ({
+//     login: (username: any, password: any) => Promise.resolve(),
+//     logout: () => Promise.resolve(),
+//     goToLoginPage: () => {},
+//     register: (countdown: any) => () => {},
+//     refresh: () => Promise.resolve(),
+//     timeToLive: () => 100,
+//   }),
+//   eventBus: () => ({
+//     publish: (eventName: any, data: any) => {
+//       console.log(`eventBus published '${eventName}' with payload:`, data);
+//     },
+//     subscribe: (eventName: any, listener: any) => {
+//       console.log(`eventBus subscribed '${eventName}' to listener:`, listener);
+//     },
+//     unsubscribe: (eventName: any, listener: any) => {
+//       console.log(`eventBus unsubscribed '${eventName}' from listener:`, listener);
+//     },
+//   }),
+//   navigation: () => ({
+//     getBreadcrumbs: (uuid: any, depth: any) => {
+//       return Promise.resolve({
+//         type: 'externalLink',
+//         title: 'Backbase',
+//         url: 'http://www.backbase.com',
+//         isCurrent: true,
+//         properties: {},
+//       });
+//     },
+//     getTree: (uuid: any, depth: any) => {
+//       return Promise.resolve({
+//         type: 'externalLink',
+//         title: 'Backbase',
+//         url: 'http://www.backbase.com',
+//         isCurrent: true,
+//         isInPath: false,
+//         properties: {},
+//         children: [],
+//       });
+//     },
+//   }),
+//   pageConfig: () => ({
+//     apiRoot: '/gateway/api',
+//     staticResourcesRoot: '/gateway/api/portal',
+//     portalName: 'backbase-wc3',
+//     pageName: 'index',
+//     currentLink: '',
+//     version: '6',
+//     locale: 'en-US',
+//   }),
+//   portalContent: () => ({
+//     get: () => {
+//       return Promise.resolve({
+//         type: ContentType.IMAGE,
+//         contentRef: '/../../assets/logo-sml.svg',
+//       });
+//     },
+//   }),
+// };
 
 export const environment: Environment = {
   production: false,
   mockProviders: mockProviders,
-  bootstrap: {
-    pageModel: pageModel,
-    services: services,
-  },
+  // bootstrap: {
+  //   pageModel: pageModel,
+  //   // services: services,
+  // },
 };
 
 /*
